0ffc8772577a695f930f5268088795d1e4bf57cf
Your Name <you@example.com>
2019-12-18 14:57:46 -0500

    Add FlatOrderedMap

---
+++ test/FlatOrderedMap_tests.cpp
@@ -0,0 +1,23 @@
+#include <ftl/catch_or_ignore.h>
+#include <ftl/flat_ordered_map.h>
+
+ADD_TEST_FUNC( FlatOrderedMap_tests )
+{
+    {
+        ftl::FlatOrderedMap<int, int> m = {{2, 3}, {1, 4}};
+        REQUIRE( m.front().first == 1 && m.size() == 2 );
+        m[-1] = 20;
+        REQUIRE( m.front().first == -1 && m.size() == 3 );
+        m.erase( -1 );
+        REQUIRE( m.front().first == 1 && m.size() == 2 );
+    }
+    {
+        ftl::FlatOrderedSet<std::string> s{"cd", "ab"};
+        REQUIRE( s.front() == "ab" );
+        s += "aa";
+
+        REQUIRE( s.front() == "aa" );
+        s.erase( "aa" );
+        REQUIRE( s.front() == "ab" );
+    }
+}
