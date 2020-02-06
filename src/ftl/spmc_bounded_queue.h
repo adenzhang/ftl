66e9634fd9c08381da83bf8109f0ea6966d806c0
Your Name <you@example.com>
2020-02-05 22:45:20 -0500

    Add BlockingQueue, SPMCQueue

---
+++ src/ftl/spmc_bounded_queue.h
@@ -0,0 +1,128 @@
+/*
+ * This file is part of the ftl (Fast Template Library) distribution (https://github.com/adenzhang/ftl).
+ * Copyright (c) 2020 Aden Zhang.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+#include <atomic>
+#include <memory>
+
+namespace ftl
+{
+
+// todo: cache line optimization
+template<class T, class AllocT = std::allocator<T>>
+class SPMCQueue
+{
+    struct Node
+    {
+        std::atomic<bool> flag; // true for having value
+        T val;
+    };
+
+public:
+    using Alloc = typename std::allocator_traits<AllocT>::template rebind_alloc<Node>;
+
+    SPMCQueue( size_t nCap, const Alloc &alloc = Alloc() ) : m_alloc( alloc ), m_data( m_alloc.allocate( nCap + 1 ) ), m_bufsize( nCap + 1 )
+    {
+        for ( size_t i = 0; i < m_bufsize; ++i )
+            m_data[i].flag.store( false );
+    }
+    ~SPMCQueue()
+    {
+        if ( m_data )
+        {
+            clear();
+            m_alloc.deallocate( m_data, m_bufsize );
+            m_data = nullptr;
+        }
+    }
+
+    /// @return false when full. Note: the front element mayb be in process of dequeue.
+    bool push( const T &val )
+    {
+        auto iEnd = m_end.load( std::memory_order_acquire ) % m_bufsize;
+        auto iNext = ( iEnd + 1 ) % m_bufsize;
+        if ( m_data[iNext].flag.load( std::memory_order_acquire ) ) // full
+            return false;
+        assert( m_data[iEnd].flag.load( std::memory_order_acquire ) == false );
+        new ( &m_data[iEnd].val ) T( val );
+        assert( !m_data[iEnd].flag );
+        m_data[iEnd].flag.store( true, std::memory_order_release );
+        m_end.fetch_add( 1 );
+        return true;
+    }
+
+    bool pop( T *val )
+    {
+        size_t ibegin = m_begin.load( std::memory_order_acquire );
+        do
+        {
+            if ( ibegin == m_end.load( std::memory_order_acquire ) ) // emtpy
+                return false;
+        } while ( !m_begin.compare_exchange_weak( ibegin, ibegin + 1 ) ); // std::memory_order_acquire
+
+        // alreay acquired the ownership.
+        ibegin %= m_bufsize;
+        auto &data = m_data[ibegin];
+        assert( data.flag.load( std::memory_order_acquire ) );
+
+        if ( val )
+            new ( val ) T( std::move( data.val ) );
+        data.val.~T();
+        data.flag.store( false, std::memory_order_release );
+        return true;
+    }
+
+    size_t size() const
+    {
+        return m_end.load() - m_begin.load();
+    }
+
+    bool empty() const
+    {
+        return m_begin.load() == m_end.load();
+    }
+
+    /// weak full. dequeue may be in process.
+    bool full() const
+    {
+        return m_begin.load() + ( m_bufsize - 1 ) == m_end.load();
+    }
+
+    void stop()
+    {
+        m_stopping = true;
+    }
+
+    bool stopping() const
+    {
+        return m_stopping;
+    }
+
+    void clear()
+    {
+        while ( pop( nullptr ) )
+            ;
+    }
+
+protected:
+    Alloc m_alloc;
+    Node *m_data = nullptr;
+    size_t m_bufsize = 0;
+    std::atomic<size_t> m_begin{0}, m_end{0};
+    std::atomic<bool> m_stopping{false};
+};
+} // namespace ftl
